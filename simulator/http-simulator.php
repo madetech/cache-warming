a0d93beeefcdeb3adbe583b6bb32c4f15e53e26e
Craig Bass <craig@madetech.com>
2016-08-22 13:33:09 +0100

    Use anchors to prevent URL routing confusion

--- simulator/http-simulator.php
+++ simulator/http-simulator.php
@@ -2,20 +2,20 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 $uri = $_SERVER['REQUEST_URI'];
 switch (true) {
-    case preg_match('~gb/en/sitemap\.xml~', $uri, $matches):
+    case preg_match('~^/gb/en/sitemap\.xml~', $uri, $matches):
         echo file_get_contents(__DIR__ . "/responses/britishSiteMap.xml");
         break;
-    case preg_match('~nl/nl/sitemap\.xml~', $uri, $matches):
+    case preg_match('~^/nl/nl/sitemap\.xml~', $uri, $matches):
         echo file_get_contents(__DIR__ . "/responses/dutchSiteMap.xml");
         break;
-    case preg_match('/sitemap([0-9]?)\.xml/', $uri, $matches):
+    case preg_match('~^/sitemap([0-9]?)\.xml~', $uri, $matches):
         $siteMapNumber = is_numeric($matches[1]) ? $matches[1] : '';
         echo file_get_contents(__DIR__ . "/responses/sitemap{$siteMapNumber}.xml");
         break;
-    case preg_match('/timeout/', $uri, $matches):
+    case preg_match('~^/timeout~', $uri, $matches):
         sleep(40);
         break;
-    case preg_match('/alive/', $uri, $matches):
+    case preg_match('~^/alive~', $uri, $matches):
         echo 'ALIVE';
         exit;
         break;
